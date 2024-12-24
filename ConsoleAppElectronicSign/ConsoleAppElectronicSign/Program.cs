429a0b8415ba2f29c92d53043d5a373be1784a7b
Brainjar60 <vito.perrotta@gmail.com>
2024-12-24 16:57:18 +0100

    Add files via upload

---
+++ ConsoleAppElectronicSign/ConsoleAppElectronicSign/Program.cs
@@ -0,0 +1,31 @@
+﻿
+using Microsoft.Office.Interop.Word;
+using Document = Microsoft.Office.Interop.Word.Document;
+namespace ConsoleAppElectronicSign
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            SaveAsPDF(); 
+        }
+        public static string ChgFileExt(string fileName, string newExt)
+        {
+            return fileName.Substring(0, fileName.LastIndexOf(".") + 1) + newExt;
+        }
+        public static void SaveAsPDF()
+        {
+            string newDocument = "C:/ProveRepos/DocumentoDaFirmareConSignHereAnchorPagina1.docx";
+            // WriteFile(byteArray, newDocument);
+            Microsoft.Office.Interop.Word.Application application = new Microsoft.Office.Interop.Word.Application();
+            Document document = null;
+            application.Visible = false;
+            // open docx file
+            document = application.Documents.Open(newDocument);
+            // convert to pdf file
+            document.ExportAsFixedFormat(ChgFileExt(newDocument, "pdf"), WdExportFormat.wdExportFormatPDF);
+            document.Close();
+        }
+    }
+}
