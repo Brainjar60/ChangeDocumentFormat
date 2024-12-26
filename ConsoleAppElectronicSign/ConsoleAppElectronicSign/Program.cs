9ce2ff798dff3d36f030a406701f03189b2ad2c5
Vito Perrotta <vito.perrotta@gmail.com>
2024-12-26 15:00:57 +0100

    Aggiunta log messages

--- ConsoleAppElectronicSign/ConsoleAppElectronicSign/Program.cs
+++ ConsoleAppElectronicSign/ConsoleAppElectronicSign/Program.cs
@@ -1,14 +1,23 @@
 ﻿
 using Microsoft.Office.Interop.Word;
+using System;
+using System.Collections;
+using System.Diagnostics; 
+using System.Net.Mail;
+using System.Reflection.Metadata;
 using Document = Microsoft.Office.Interop.Word.Document;
+using MailMessage = System.Net.Mail.MailMessage;
 namespace ConsoleAppElectronicSign
 {
-    internal class Program
+    public class Program
     {
+        private static string attDocument = "";
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
-            SaveAsPDF(); 
+            SaveAsPDF();
+            SendMail(); 
         }
         public static string ChgFileExt(string fileName, string newExt)
         {
@@ -16,7 +25,7 @@ namespace ConsoleAppElectronicSign
         }
         public static void SaveAsPDF()
         {
-            string newDocument = "C:/ProveRepos/DocumentoDaFirmareConSignHereAnchorPagina1.docx";
+            string newDocument = "E:/ProveRepos/DocumentoDaFirmareConSignHereAnchorPagina1.docx";
             // WriteFile(byteArray, newDocument);
             Microsoft.Office.Interop.Word.Application application = new Microsoft.Office.Interop.Word.Application();
             Document document = null;
@@ -24,8 +33,97 @@ namespace ConsoleAppElectronicSign
             // open docx file
             document = application.Documents.Open(newDocument);
             // convert to pdf file
-            document.ExportAsFixedFormat(ChgFileExt(newDocument, "pdf"), WdExportFormat.wdExportFormatPDF);
+            attDocument = ChgFileExt(newDocument, "pdf");
+            document.ExportAsFixedFormat(attDocument, WdExportFormat.wdExportFormatPDF);
             document.Close();
+            application.Quit();
+            application = null; 
         }
+        public static void SendMail()
+        {
+            try
+            {
+                MailMessage mail = new MailMessage();
+                SmtpClient SmtpServer = new SmtpClient("smtp.atvinformatica.it", 25);
+                SmtpServer.UseDefaultCredentials = false;
+                mail.DeliveryNotificationOptions = DeliveryNotificationOptions.OnSuccess | DeliveryNotificationOptions.OnFailure;
+                SmtpServer.Credentials = new System.Net.NetworkCredential("postmaster@atvinformatica.it", "ArubaPassword$0");
+                SmtpServer.EnableSsl = false;
+                mail.From = new MailAddress("postmaster@atvinformatica.it");
+                ArrayList emailingList = new ArrayList();
+                emailingList.Add("vito.perrotta@ipsedocet.it");
+                emailingList.Add("v.perrotta@dacomat.com");
+                emailingList.Add("vito.perrotta@gmail.com");
+                foreach (string oTo in emailingList)
+                {
+                    mail.To.Add(oTo);
+                }
+                mail.CC.Add("vperrotta@alveria.it");
+                mail.Attachments.Add(new Attachment(attDocument));
+                mail.Subject = "Invio programma SMT";
+                mail.Body = "messaggio";
+                SmtpServer.Send(mail);
+                Debug.WriteLine("Mail spedita correttamente");
+                Console.WriteLine("Mail spedita correttamente");
+
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                Console.WriteLine(ex.Message);
+            }
+        }
+        /// <summary>
+        /// Metodo da inserire in un controller ASPX
+        /// </summary>
+        /// <param name="envelopeId"></param>
+        /// <param name="documentId"></param>
+        /// <param name="documentName"></param>
+        /// <returns></returns>
+        public string GetEnvelopeDocument(string envelopeId, string documentId, string documentName)
+        {
+            Stream streamVal = null; // Acuisire lo stream del documento
+            string fileName = "c:/Temp/" + documentName + "_" + envelopeId + ".pdf";
+            //saveTemporaryFile(fileName, streamVal); 
+            int iB = (int)streamVal.Length;
+            BinaryReader reader = new BinaryReader(streamVal);
+            byte[] pdfBytes = reader.ReadBytes(iB);
+            string docBase64 = "" + Convert.ToBase64String(pdfBytes);
+            return docBase64;
+        }
+        // ------------------------------------------------------------------------------------
+        // funzione Javascript che legge il documento tramite metodo controller e lo visualizza
+        // in una finestra della applicazione host 
+        // ------------------------------------------------------------------------------------
+        /*
+        function invokeAjaxGetEnvelopeCertificate(envelopeId, documentId, documentName)
+        {
+            var xhr = new XMLHttpRequest();
+            var _url = 'api/adlinker/ajaxGetEnvelopeDocument?envelopeId=' + envelopeId + "&documentId=" + documentId + "&documentName=" + documentName;
+            xhr.onreadystatechange = function() {
+                if (this.readyState == 4 && this.status == 200)
+                {
+                    if (!this.response || this.response == null || this.response == "")
+                    {
+                        return;
+                    }
+                    console.log("ajax response" + documentName);
+
+                    let pdfWindow = window.open("Certificate of Completion", "Certificate", "titlebar=yes,toolbar=yes,scrollbars=yes,resizable=yes,top=10,left=10,width=1725,height=865");
+                    pdfWindow.document.write(
+                        "<iframe width='100%' height='100%' src='data:application/pdf;base64, " +
+                        this.response + "'></iframe>"
+                    )
+
+                }
+                else
+                {
+                    return;
+                }
+            };
+            xhr.open('get', _url);
+            xhr.send();
+        }
+        */
     }
 }
--- ConsoleAppElectronicSign/ConsoleAppElectronicSign/obj/Debug/net6.0/ConsoleAppElectronicSign.AssemblyInfo.cs
+++ ConsoleAppElectronicSign/ConsoleAppElectronicSign/obj/Debug/net6.0/ConsoleAppElectronicSign.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleAppElectronicSign")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66500d40e0f162a20df311a7c5fc453f4025280b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleAppElectronicSign")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleAppElectronicSign")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
